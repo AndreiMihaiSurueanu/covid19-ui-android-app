75b0b28ba3dc9e6a64db353eea84897644b96295
andrei <git1@email.com>
2021-11-07 19:03:56 +0100

    Linked Details activity to Main activity

--- app/src/main/java/com/example/covid19uiandroidapp/MainActivity.java
+++ app/src/main/java/com/example/covid19uiandroidapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.covid19uiandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout confirmed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        confirmed = findViewById(R.id.confirm);
+
+        confirmed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this, Details.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
